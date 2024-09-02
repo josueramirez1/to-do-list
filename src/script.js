9a2841eda36fc8eadc4c33657599e184b8d36de5
josueramirez1 <josue67ramirez@gmail.com>
2024-09-02 00:03:48 -0700

    creating website ui to see default list and tabs
    providing visual to how user will interact with webpage.

--- src/script.js
+++ src/script.js
@@ -1,46 +1,18 @@
-// class Item {
-//   // This function creates an object. The object is an item that contains a message, checkbox, downvote, upvote and delete button
-//   constructor(title, description, priority) {
-//     this.title = title;
-//     this.description = description;
-//     this.dueDate = dueDate;
-//     this.priority = priority;
-//   }
-// }
-
-// function createTodoItem() {
-//   const content = document.querySelector("#content");
-//   const todoItem = document.createElement("div");
-//   todoItem.classList.add("todo-item");
-//   const newItem = new Item("General", "Go to the store", "today", "high");
-//   console.log(newItem.title);
-//   todoItem.textContent = newItem.title;
-//   content.appendChild(todoItem);
-// }
-
-// createTodoItem();
-
-class InboxTab {
-  constructor(inboxTitle) {
-    this.tabTitle = tabTitle;
+class List {
+  constructor(listTitle) {
+    this.listTitle = listTitle;
   }
 }
 
-function createHub() {
-  const content = document.querySelector("#content");
-  const firstColumn = document.createElement("div");
-  const secondColumn = document.createElement("div");
-  content.appendChild(firstColumn);
-  content.appendChild(secondColumn);
-
-  firstColumn.classList.add("first-column");
-  secondColumn.classList.add("second-column");
-
-  const createGeneralTabs = () => {
-    const inbox = new Tab("Inbox");
-    console.log(inbox);
-    firstColumn.appendChild(inbox.tabTitle);
-  };
-  createGeneralTabs();
+class Todo {
+  // This function creates an object. The object is an item that contains a message, checkbox, downvote, upvote and delete button
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
 }
-createHub();
+
+const today = new List("Today");
+console.log(today);
