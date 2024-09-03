6babce1c7d8a2301409cabe4eda49278d748e220
josueramirez1 <josue67ramirez@gmail.com>
2024-09-03 13:44:45 -0700

    styling checkbox and changing format to inputs and putting flexbox. It allows dynamic sizing

--- src/index.js
+++ src/index.js
@@ -11,14 +11,15 @@ function createTodo() {
     "high"
   );
   const currentListItem = document.querySelector(".current-list-item");
-  // input and attributes
+  // div that contains label and p
+  const currentListText = document.createElement("div");
+  currentListText.classList.add("current-list-text");
+  // input
+
   const input = document.createElement("input");
   input.setAttribute("type", "checkbox");
   input.setAttribute("name", "to-do");
   input.setAttribute("id", "to-do");
-  // div that contains label and p
-  const currentListText = document.createElement("div");
-  currentListText.classList.add("current-list-text");
   // label
   const label = document.createElement("label");
   label.setAttribute("for", "to-do");
@@ -29,8 +30,9 @@ function createTodo() {
 createTodo();
 
 {
-  /* <input type="checkbox" name="to-do" id="to-do" />
+  /*
             <div class="current-list-text">
+            <input type="checkbox" name="to-do" id="to-do" />
               <label for="to-do" class="list-item">Add to-do</label>
               <p class="list-description">Type description...</p>
             </div> */
