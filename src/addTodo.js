9022f6c47d017ee8b6096541dd2cc54068a6a96d
josueramirez1 <josue67ramirez@gmail.com>
2024-09-05 16:23:18 -0700

    adding styling and adding a todo js just in case. creating helper function
    creating helper functions to help sepearte the code better and maintain readability.

---
+++ src/addTodo.js
@@ -0,0 +1,17 @@
+export default function addTodo(e) {
+  //   console.log(e.target);
+  //   const secondColumnBody = document.querySelector(".second-column-body");
+  //   secondColumnBody.addEventListener("click", (e) => {
+  //     if (
+  //       e.target.matches(".list-item") ||
+  //       e.target.matches(".list-description") ||
+  //       e.target.matches(".current-list-item")
+  //     ) {
+  //       return;
+  //     }
+  //     const currentListItemActive = document.querySelector(".active");
+  //     if (currentListItemActive) {
+  //       currentListItemActive.classList.remove("active");
+  //     }
+  //   });
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
 import "./css/styles.css";
-import List from "./list.js";
 import Todo from "./todo.js";
 
-document.addEventListener("click", (e) => {
-  if (e.target.matches(".second-column-body")) Todo.createTodo();
-  const title = document.querySelector(".list-item");
-  const description = document.querySelector(".list-description");
-  const array = [];
-  title.addEventListener("change", (e) => {
-    let newTitle = title.value;
-    const newTodo = new Todo(newTitle);
-    console.log(newTodo.title);
+function start() {
+  const secondColumnBody = document.querySelector(".second-column-body");
+  initialTodo();
+  secondColumnBody.addEventListener("click", (e) => {
+    activeTodo(e);
+    inactiveTodo(e);
   });
-  // description.addEventListener("change", (e) => {
-  //   let newDescription = description.value;
-  // });
-});
+}
+
+function initialTodo() {
+  // select parent
+  const currentListItems = document.querySelector(".current-list-items");
+  // if list does not have an item, add one
+  if (!currentListItems.hasChildNodes()) Todo.createTodo();
+}
+
+function activeTodo(e) {
+  if (e.target.matches(".list-item")) {
+    e.target.closest(".current-list-item").classList.remove("inactive");
+    e.target.closest(".current-list-item").classList.add("active");
+    e.target.closest(
+      ".list-todo-checkbox"
+    ).nextElementSibling.style.visibility = "visible";
+  }
+}
+
+function inactiveTodo(e) {
+  const currentList = [...document.querySelectorAll(".current-list-item")];
+  if (e.target.matches(".second-column-body")) {
+    currentList.forEach((item) => {
+      item.classList.add("inactive");
+      item.classList.remove("active");
+      hideNotes();
+    });
+  }
+}
+
+function hideNotes() {
+  const itemGrandchildren = [
+    ...document.querySelector(".current-list-text").children,
+  ];
+  for (let todo of itemGrandchildren) {
+    if (todo.matches(".list-todo-description-box")) {
+      todo.style.visibility = "hidden";
+    }
+  }
+}
+
+start();
