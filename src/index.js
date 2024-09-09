fd270575af8b8385b2056be63120b13855bf09ed
josueramirez1 <josue67ramirez@gmail.com>
2024-09-09 15:52:40 -0700

    completed creating and adding todo functions which allows user to click in white space for new todo and to make correction on old todo without adding a new one.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import Todo from "./todo.js";
 function start() {
   const secondColumnBody = document.querySelector(".second-column-body");
   const currentListDiv = document.querySelector(".current-list-items");
+  let listItemDiv = [...document.querySelectorAll(".list-item")];
 
   secondColumnBody.addEventListener("click", (e) => {
     let currentListItems = [...currentListDiv.children];
-    createTodo(e, currentListDiv, currentListItems);
+    let listItemDiv = [...document.querySelectorAll(".list-item")];
 
-    // document.addEventListener("click", addNewTodo(currentListItems));
+    createTodo(e, currentListDiv, currentListItems);
+    activeTodo(e);
   });
 }
 
@@ -19,43 +21,44 @@ function createTodo(e, currentListDiv, currentListItems) {
     Todo.createTodo();
     return;
   }
-  // engine loops through each item
 
+  // engine begins to look at array to see if every item has a class of inactive. If this is true, then it will create a new todo.
   let inactivity = currentListItems.every((item) => {
     return item.matches(".inactive");
   });
-
+  // engine looks through array to find item that contains the active class. If this is true, then a function will fire which will remove the active todo and remove visibility.
   let activity = currentListItems.find((item) => {
     return item.matches(".active");
   });
 
-  if (inactivity) Todo.createTodo();
+  if (inactivity && !e.target.matches(".list-item")) Todo.createTodo();
   if (activity) {
     currentListItems.forEach((item) => {
       inactiveTodo(item);
     });
   }
+  if (e.target.matches(".list-item")) activeTodo(e);
 }
 
 function activeTodo(e) {
   if (e.target.matches(".list-item")) {
+    console.log("yes");
     e.target.closest(".current-list-item").classList.remove("inactive");
     e.target.closest(".current-list-item").classList.add("active");
     e.target.closest(
       ".list-todo-checkbox"
     ).nextElementSibling.style.visibility = "visible";
   }
+  return;
 }
 
 function inactiveTodo(item) {
   item.classList.add("inactive");
   item.classList.remove("active");
-  let itemGrandchildren = [
-    ...document.querySelector(".current-list-text").children,
+  let todoDescriptionBox = [
+    ...document.querySelectorAll(".list-todo-description-box"),
   ];
-  itemGrandchildren.forEach((i) => {
-    if (i.matches(".list-todo-description-box")) i.style.visibility = "hidden";
-  });
+  todoDescriptionBox.forEach((i) => (i.style.visibility = "hidden"));
 }
 
 start();
